fe0f3d543ab59f5dbb03e2b7c0347e7fbe36ba09
TetsuyaKimura <t.kimura4510@gmail.com>
2020-07-24 16:13:08 +0900

    経路探索のバグ修正。

--- ProjectTenko/ProjectTenko/ProjectTenko/Src/NavigationAI/Node.cpp
+++ ProjectTenko/ProjectTenko/ProjectTenko/Src/NavigationAI/Node.cpp
@@ -4,5 +4,11 @@ void Route::AddPos(std::vector<D3DXVECTOR3>& route_, float size_) const
 {
 	D3DXVECTOR3 vec(m_Node->m_Cell.m_Column * size_, 0.0f, m_Node->m_Cell.m_Row);
 	route_.push_back(vec);
+
+	if (m_Parent == nullptr)
+	{
+		return;
+	}
+
 	m_Parent->AddPos(route_, size_);
 }
